f4c83d6d2f2dcc42ac80f3645f25f3ef5af77b4e
rick1128 <111245831+rick1128@users.noreply.github.com>
2022-08-24 15:14:57 +0300

    Update __init__.py

--- iqqhtani/__init__.py
+++ iqqhtani/__init__.py
@@ -17,7 +17,7 @@ if Config.PRIVATE_GROUP_BOT_API_ID == 0:
         Config.BOTLOG = False
         Config.BOTLOG_CHATID = "me"
     
-        Config.BOTLOG_CHATID = int(gvarstatus("PRIVATE_GROUP_BOT_API_ID"))
+        
         Config.PRIVATE_GROUP_BOT_API_ID = int(gvarstatus("PRIVATE_GROUP_BOT_API_ID"))
         Config.BOTLOG = True
 else:
