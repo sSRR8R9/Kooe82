d8f6c64db67527458ccef4184ded3e576febe3a5
yah737dh <99218321+yah737dh@users.noreply.github.com>
2022-08-26 23:28:52 +0300

    Update __main__.py

--- iqqhtani/__main__.py
+++ iqqhtani/__main__.py
@@ -1,30 +1,19 @@
-
 import sys
-import userbot
-from userbot import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
+import Arab
+from Arab import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
 from telethon import functions
 from .Config import Config
 from .core.logger import logging
 from .core.session import iqthon
 from .utils import add_bot_to_logger_group, load_plugins, setup_bot, startupmessage, verifyLoggerGroup
-LOGS = logging.getLogger(
-"ريك ثون"
-)
-print(
-userbot.__copyright__)
-print(
-"المرخصة بموجب شروط " + userbot.__license__)
+LOGS = logging.getLogger("ريك ثون")
 cmdhr = Config.COMMAND_HAND_LER
 try:
-    LOGS.info(
-"بدء تنزيل ريك ثون"
-)
-    iqthon.loop.run_until_complete(
-setup_bot())
+    LOGS.info("بدء تنزيل ريك ثون")
+    iqthon.loop.run_until_complete(setup_bot())
     LOGS.info("بدء تشغيل البوت")
 except Exception as e:
-    LOGS.error(
-f"{str(e)}")
+    LOGS.error(f"{str(e)}")
     sys.exit()
 class CatCheck:
     def __init__(self):
@@ -34,9 +23,7 @@ async def startup_process():
     await verifyLoggerGroup()
     await load_plugins("plugins")
     await load_plugins("assistant")
-    print(
-f"<b> ⌔︙ اهلا بك لقد نصبت ريك ثون بنجاح 🥁 اذهب الى قناتنا لمعرفة المزيـد ⤵️. </b>\n CH : https://t.me/rickthon"
-
+    print(f"<b> ⌔︙ اهلا بك لقد نصبت ريك ثون بنجاح 🥁 اذهب الى قناتنا لمعرفة المزيـد ⤵️. </b>\n CH : https://t.me/rickthon ")
     await verifyLoggerGroup()
     await add_bot_to_logger_group(BOTLOG_CHATID)
     if PM_LOGGER_GROUP_ID != -100:
@@ -47,38 +34,17 @@ f"<b> ⌔︙ اهلا بك لقد نصبت ريك ثون بنجاح 🥁 اذه�
 iqthon.loop.run_until_complete(startup_process())
 def start_bot():
   try:
-    iqthon.loop.run_until_complete(
-iqthon(
-functions.channels.JoinChannelRequest
-(
-"rickthon"
-))
-)
-    iqthon.loop.run_until_complete(
-iqthon(
-functions.channels.JoinChannelRequest("ClashJOKER7X"
-)
-))
-    iqthon.loop.run_until_complete(
-iqthon(
-functions.channels.JoinChannelRequest(
-"rickthons"
-)))
-    iqthon.loop.run_until_complete(
-iqthon(
-functions.channels.JoinChannelRequest(
-"SO_SELVA"
-)))  
+      List = ["iqthon","uruur"]
+      for id in List :
+          iqthon.loop.run_until_complete(iqthon(functions.channels.JoinChannelRequest(id)))
   except Exception as e:
     print(e)
     return False
 Checker = start_bot()
 if Checker == False:
-    print(
-"عذرا لديك حظر مؤقت حاول التنصيب غدا او بعد 24 ساعة"
-)
-    iqthon.disconnect()
-    sys.exit()
+    print("كتمل تنصيب #1")
+    
+
 if len(sys.argv) not in (1, 3, 4):
     iqthon.disconnect()
 elif not Catcheck.sucess:
@@ -89,4 +55,3 @@ else:
         iqthon.run_until_disconnected()
     except ConnectionError:
         pass
-    
