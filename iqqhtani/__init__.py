3303214018e14e786bf677cd132c0be9cbcd8bfc
yah737dh <99218321+yah737dh@users.noreply.github.com>
2022-08-26 23:03:13 +0300

    Update __init__.py

--- iqqhtani/__init__.py
+++ iqqhtani/__init__.py
@@ -4,18 +4,18 @@ from .Config import Config
 from .core.logger import logging
 from .core.session import iqthon
 from .sql_helper.globals import addgvar, delgvar, gvarstatus
-__version__ = "7.5"
+__version__ = "7.6"
 __license__ = "GNU Affero General Public License v3.0"
-__author__ = "<t.me/J0KER_7x>"
-__copyright__ = "telethon 7x (C) 2021 - 2022  " + __author__
+__author__ = "<t.me/iqthon>"
+__copyright__ = "telethon AR (C) 2020 - 2021  " + __author__
 iqthon.version = __version__
 iqthon.tgbot.version = __version__
 LOGS = logging.getLogger("IQTHON")
 bot = iqthon
 StartTime = time.time()
-catversion = "7.2"
-if Config.UPSTREAM_REPO == "TelethonJOKER7x":
-    UPSTREAM_REPO_URL = "https://github.com/telethon-Arab/telethonNow"
+catversion = "7.6"
+if Config.UPSTREAM_REPO == "TelethonAr":
+    UPSTREAM_REPO_URL = "https://github.com/telethonArab/TelethonAr1"
 else:
     UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
 if Config.PRIVATE_GROUP_BOT_API_ID == 0:
