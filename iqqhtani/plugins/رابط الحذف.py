46ff5b6c053729cc891a6d7c7b8de6fa0f0576b9
rick1128 <111245831+rick1128@users.noreply.github.com>
2022-08-29 00:44:27 +0300

    Update رابط الحذف.py

--- iqqhtani/plugins/رابط الحذف.py
+++ iqqhtani/plugins/رابط الحذف.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 from telethon.errors.rpcerrorlist import MediaEmptyError
-from iqqhtani import rickthon
+from ..core.session import rickthon
 from ..core.managers import edit_or_reply
 from ..helpers.google_image_download import googleimagesdownload
 from ..helpers.utils import reply_id
