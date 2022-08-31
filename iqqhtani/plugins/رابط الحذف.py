2b352f4b412cf3d71818fd5a2a827cd9e77fcd94
rick1128 <111245831+rick1128@users.noreply.github.com>
2022-08-31 15:46:16 +0300

    Update رابط الحذف.py

--- iqqhtani/plugins/رابط الحذف.py
+++ iqqhtani/plugins/رابط الحذف.py
@@ -1,11 +1,11 @@
 import os
 import shutil
 from telethon.errors.rpcerrorlist import MediaEmptyError
-from Arab import iqthon
+from ..core.session import rickthon
 from ..core.managers import edit_or_reply
 from ..helpers.google_image_download import googleimagesdownload
 from ..helpers.utils import reply_id
-plugin_category = "@iqthon"
-@iqthon.iq_cmd(pattern="رابط الحذف")
+plugin_category = "@rickthon"
+@rickthon.iq_cmd(pattern="رابط الحذف")
 async def _(kno):
     await edit_or_reply (kno, "**رابـط الحـذف ↬** https://telegram.org/deactivate \n\n ** بـوت الحـذف  ↬** @LC6BOT ")
