a8dfcdd550e3d7fbe8a394a98d6906fe6c0c1753
rick1128 <111245831+rick1128@users.noreply.github.com>
2022-09-06 01:04:47 +0300

    Add files via upload

--- iqqhtani/plugins/رابط الحذف.py
+++ iqqhtani/plugins/رابط الحذف.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 from telethon.errors.rpcerrorlist import MediaEmptyError
-from ..core.session import rickthon
+from iqqhtani import rickthon
 from ..core.managers import edit_or_reply
 from ..helpers.google_image_download import googleimagesdownload
 from ..helpers.utils import reply_id
