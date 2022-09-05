db3ff8aa93c1ff9824644bbf05ae597b187d06da
yah737dh <99218321+yah737dh@users.noreply.github.com>
2022-09-06 02:33:54 +0300

    Add files via upload

--- iqqhtani/plugins/رابط الحذف.py
+++ iqqhtani/plugins/رابط الحذف.py
@@ -1,11 +1,7 @@
-import os
-import shutil
-from telethon.errors.rpcerrorlist import MediaEmptyError
 from iqqhtani import rickthon
 from ..core.managers import edit_or_reply
-from ..helpers.google_image_download import googleimagesdownload
 from ..helpers.utils import reply_id
 plugin_category = "@rickthon"
 @rickthon.iq_cmd(pattern="رابط الحذف")
-async def _(kno):
-    await edit_or_reply (kno, "**رابـط الحـذف ↬** https://telegram.org/deactivate \n\n ** بـوت الحـذف  ↬** @LC6BOT ")
+async def _(rick):
+    await edit_or_reply (rick, "**رابـط الحـذف ↬** https://telegram.org/deactivate \n\n ** بـوت الحـذف  ↬** @LC6BOT ")
