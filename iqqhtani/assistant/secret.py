34e078d8a47eaa0a26ecebaface68f22541cbb17
yah737dh <99218321+yah737dh@users.noreply.github.com>
2022-09-06 22:28:50 +0300

    Add files via upload

--- iqqhtani/assistant/secret.py
+++ iqqhtani/assistant/secret.py
@@ -4,25 +4,25 @@ import re
 
 from telethon.events import CallbackQuery
 
-from iqqhtani import iqqhtani
+from iqqhtani import rickthon
 
 
-@iqqhtani.tgbot.on(CallbackQuery(data=re.compile(b"secret_(.*)")))
+@rickthon.tgbot.on(CallbackQuery(data=re.compile(b"secret_(.*)")))
 async def on_plug_in_callback_query_handler(event):
     timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
-    if os.path.exists("./iqqhtani/secrets.txt"):
-        jsondata = json.load(open("./iqqhtani/secrets.txt"))
+    if os.path.exists("./rickthon/secrets.txt"):
+        jsondata = json.load(open("./rickthon/secrets.txt"))
         try:
             message = jsondata[f"{timestamp}"]
             userid = message["userid"]
-            ids = [userid, iqqhtani.uid]
+            ids = [userid, rickthon.uid]
             if event.query.user_id in ids:
                 encrypted_tcxt = message["text"]
                 reply_pop_up_alert = encrypted_tcxt
             else:
                 reply_pop_up_alert = "why were you looking at this shit go away and do your own work, idiot"
         except KeyError:
-            reply_pop_up_alert = "This message no longer exists in iqqhtani server"
+            reply_pop_up_alert = "This message no longer exists in rickthon server"
     else:
         reply_pop_up_alert = "This message no longer exists "
     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
