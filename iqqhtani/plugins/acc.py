883ba90cffc14d2f274a6a3c38d57b0702f390c3
rick1128 <111245831+rick1128@users.noreply.github.com>
2022-09-07 23:52:53 +0300

    Delete acc.py

--- iqqhtani/plugins/acc.py
+++
@@ -1,5 +0,0 @@
-@rickthon.on(events.NewMessage(outgoing=True, pattern=r"تيك توكك"))
-async def i(event):
-    ac = open("accx.txt","r")
-    acc = ac.readlines()[random.randint(1,366)]
-    await rickthon.send_message(event.chat_id,f"• لديك حسابك الان ..\n• {acc}")
