b571b01f4ae86438afdffd8728a627f9ecb7eed5
yah737dh <99218321+yah737dh@users.noreply.github.com>
2022-09-09 21:04:30 +0300

    Update startup.py

--- iqqhtani/utils/startup.py
+++ iqqhtani/utils/startup.py
@@ -125,8 +125,7 @@ async def verifyLoggerGroup():
         _, groupid = await create_supergroup(            "تخزين ريك ثون الخاص", rickthon, Config.TG_BOT_USERNAME, descript    , rickphoto2  )
         addgvar("PM_LOGGER_GROUP_ID", groupid)
         print("♛ ︙ تم إنشاء مجموعة خاصة لـ PRIVATE_GROUP_BOT_API_ID بنجاح وإضافتها إلى المتغيرات.")
-    else:
-           await rickthon(JoinChannelRequest(rickthonvars))
+        await rickthon(JoinChannelRequest(rickthonvars))
         flag = True
     if flag:
         executable = sys.executable.replace(" ", "\\ ")
