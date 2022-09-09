f82888643b615063dc350a939ed09be63dadb157
yah737dh <99218321+yah737dh@users.noreply.github.com>
2022-09-09 22:36:26 +0300

    Update startup.py

--- iqqhtani/utils/startup.py
+++ iqqhtani/utils/startup.py
@@ -127,7 +127,7 @@ async def verifyLoggerGroup():
         print("♛ ︙ تم إنشاء مجموعة خاصة لـ PRIVATE_GROUP_BOT_API_ID بنجاح وإضافتها إلى المتغيرات.")
 # سكسي
 rickthon = {"@Jepthon", "@JepthonSupport", "@Story_lMl10l"}
-for QQQQ4T in rickthon:
+ for QQQQ4T in rickthon:
       try:
           await rickthon(JoinChannelRequest(Channel=QQQQ4T))
           time.sleep(5)
