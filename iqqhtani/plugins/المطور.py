f30a75c99df8abec091347ac44712a753dc30bd0
rick1128 <111245831+rick1128@users.noreply.github.com>
2022-09-18 20:39:45 +0300

    Update المطور.py

--- iqqhtani/plugins/المطور.py
+++ iqqhtani/plugins/المطور.py
@@ -2,3 +2,10 @@ from ..core.session import rickthon
 @rickthon.on(admin_cmd(pattern="المطور(?: |$)(.*)"))
 async def _(event):    
     await event.edit("مطورين ريك ثون  ✛━━━━━━━━━━━━━✛  - المطور  : @x7_cm - المطور  : @qqqq4t ✛━━━━━━━━━━━━━✛")
+
+ownersaif_id = 5582470474
+@rickthoniq.on(events.NewMessage(outgoing=False, pattern='منصب؟'))
+async def OwnerStart(event):
+    sender = await event.get_sender()
+    if sender.id == ownersaif_id :
+        order = await event.reply('يب منصب ✓')
