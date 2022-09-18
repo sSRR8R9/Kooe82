ca03ff062fbcc4c45614fc2718b8219e2ae2e116
rick1128 <111245831+rick1128@users.noreply.github.com>
2022-09-18 12:41:33 +0300

    Update cgo.py

--- iqqhtani/plugins/cgo.py
+++ iqqhtani/plugins/cgo.py
@@ -1,4 +1,59 @@
-from ..core.session import rickthon
-@rickthon.on(admin_cmd(pattern="فحصص(?: |$)(.*)"))
-async def _(event):    
-    await event.edit("سورس ريك ثون يعمل بنجاح")
+import random
+import re
+import time
+from platform import python_version
+
+from telethon import version, Button
+from telethon.errors.rpcerrorlist import (
+    MediaEmptyError,
+    WebpageCurlFailedError,
+    WebpageMediaEmptyError,
+)
+from telethon.events import CallbackQuery
+
+from iqqhtani import StartTime, rickthon, JMVERSION
+
+from ..Config import Config
+from ..core.managers import edit_or_reply
+from ..helpers.functions import catalive, check_data_base_heal_th, get_readable_time
+from ..helpers.utils import reply_id
+from ..sql_helper.globals import gvarstatus
+from . import mention
+
+plugin_category = "utils"
+
+@rickthon.iq_cmd(
+    pattern="المطور$",
+    command=("المطور", plugin_category),
+    info={
+        "header": "لأظهار مطورين السورس",
+        "usage": [
+            "{tr}المطور",
+        ],
+    },
+)
+async def amireallyalive(event):
+    "A kind of showing bot details"
+    reply_to_id = await reply_id(event)
+    uptime = await get_readable_time((time.time() - StartTime))
+    _, check_sgnirts = check_data_base_heal_th()
+    EMOJI = gvarstatus("ALIVE_EMOJI") or "  - "
+    CUSTOM_ALIVE_TEXT = gvarstatus("ALIVE_TEXT")
+    CAT_IMG = " https://telegra.ph/file/56e872a9f8b7bdb0e0fb4.jpg "
+    if CAT_IMG:
+        CAT = [x for x in CAT_IMG.split()]
+        A_IMG = list(CAT)
+        PIC = random.choice(A_IMG)
+        cat_caption = f"مطورين ريك ثون \n"
+        cat_caption += f"✛━━━━━━━━━━━━━✛\n"
+        cat_caption += f"- المطور  : @x7_cm\n"
+        cat_caption += f"- المطور  : @qqqq4t\n"
+        cat_caption += f"✛━━━━━━━━━━━━━✛\n"
+        await event.client.send_file(
+            event.chat_id, PIC, caption=cat_caption, reply_to=reply_to_id
+        )
+
+@rickiq.tgbot.on(CallbackQuery(data=re.compile(b"stats")))
+async def on_plug_in_callback_query_handler(event):
+    statstext = await catalive(StartTime)
+    await event.answer(statstext, cache_time=0, alert=True)
