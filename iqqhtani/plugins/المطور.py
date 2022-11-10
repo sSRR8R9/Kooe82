c3d684f8a883de83fe11587938ad09d2885c1656
sSRR8R9 <117201248+sSRR8R9@users.noreply.github.com>
2022-11-10 18:02:05 +0300

    Update المطور.py

--- iqqhtani/plugins/المطور.py
+++ iqqhtani/plugins/المطور.py
@@ -1,5 +1,5 @@
 from ..core.session import rickthon
 @rickthon.on(admin_cmd(pattern="المطور(?: |$)(.*)"))
 async def _(event):    
-    await event.edit("مطورين ريك ثون  ✛━━━━━━━━━━━━━✛  - المطور  : @x7_cm - المطور  : @qqqq4t ✛━━━━━━━━━━━━━✛")
+    await event.edit(" مـطـور الـسـورس  ✛━━━━━━━━━━━━━✛  -   : @rr8r3 - المطور  :  ✛━━━━━━━━━━━━━✛")
 
